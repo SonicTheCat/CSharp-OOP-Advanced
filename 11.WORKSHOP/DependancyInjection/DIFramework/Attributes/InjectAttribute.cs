f92090dde59268064a70da77184b23ae7bcb12ce
SonicTheCat <paveliliev90@gmail.com>
2018-12-11 01:54:25 +0200

    WORKSHOP - DEPENDANCY INJENCTION CUSTOM FRAMEWORK

---
+++ 11.WORKSHOP/DependancyInjection/DIFramework/Attributes/InjectAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace CurstomFrameworkDI.Attributes
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Constructor | AttributeTargets.Field, AllowMultiple = true)]
+    public class InjectAttribute : Attribute
+    {
+
+    }
+}
