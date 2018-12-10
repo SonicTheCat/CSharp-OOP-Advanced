e0c722d3cc9952d2d8bb25ad437911b57ff6999f
SonicTheCat <paveliliev90@gmail.com>
2018-12-11 01:58:26 +0200

    WORKSHOP - DEPENDANCY INJECTION CUSTOM FRAMEWORK

---
+++ 11.WORKSHOP/DIFramework/Modules/DependencyInjector.cs
@@ -0,0 +1,13 @@
+﻿using CurstomFrameworkDI.Modules.Contracts;
+
+namespace CurstomFrameworkDI.Modules
+{
+    public class DependencyInjector
+    {
+        public static Injector CreateInjector(IModule module)
+        {
+            module.Configure();
+            return new Injector(module); 
+        }
+    }
+}
